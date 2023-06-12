ed5d81dd5e0518ce48a12b37f6b957befb5e1066
Amirkh1376 <45887515+Amirkh1376@users.noreply.github.com>
2023-06-12 21:51:17 +0330

    Add files from app

---
+++ subtraction_V1.py
@@ -0,0 +1,10 @@
+# Initialize the sub variable
+sub = 0
+
+# Loop through numbers from 1 to 10
+for num in range(1, 11):
+    # subtracte  the current number from the sub
+    sub -= num
+
+# Print the final subtraction 
+print("The subtraction of numbers from 1 to 10 is:", sub)
