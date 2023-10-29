fda1b71ab7257c8f76c48cfb9c312318ac293121
Amirkh1376 <45887515+Amirkh1376@users.noreply.github.com>
2023-10-29 13:20:57 +0330

    Create file

---
+++ a_b/For_Loop/4_for.py
@@ -0,0 +1,44 @@
+##for i in range (5):
+##    print("Amir_" + str(i))
+
+##for i in range (1 , 8):
+##    print("Amir_" + str(i))
+
+##for i in range (1 , 8 , 2):
+##    print("Amir_" + str(i))
+
+str1 = "Amirhossein"
+for i in str1:
+   print(i)
+
+##str1 = "Amirhossein"
+##for i in str1:
+##    print(i , end = ' ')
+
+##str1 = "Amirhossein Khalouei"
+##print(str1[5 : 8])
+
+##str1 = "Amirhossein Khalouei"
+##for i in range(1 , 21):
+##    print(str1[0 : i])
+
+##str1 = input("Enter Full Name : ")
+##for i in range(1 , len(str1) + 1):
+##    print(str1[0 : i])
+
+# for i in range(1 , 11):
+#    print(i * "*")
+# for i in range(9 , 0 , -1):
+#    print(i * "*")
+
+# n = input("Enter Row Number : ")
+# if (n.isnumeric()):
+#     n = int(n)
+#     for i in range(1 , n + 1):
+#         print(i * "*")
+#     for i in range(n - 1 , 1 , -1):
+#         print(i * "*")
+#     else:
+#         print("END")
+# else:
+#     print("Error")
