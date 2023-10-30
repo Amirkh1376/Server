1ce7b24c190cf70730e6bc07a5feecf66d515264
Amirkh1376 <45887515+Amirkh1376@users.noreply.github.com>
2023-10-30 09:09:53 +0330

    Create file

---
+++ a_b/Test/numpy_test.py
@@ -0,0 +1,16 @@
+import BB
+import CC
+import numpy as np
+import requests
+
+# Create a NumPy array
+arr = np.array([1, 2, 3, 4, 5])
+
+# Perform basic operations
+mean_value = np.mean(arr)  # Calculate the mean of the array
+sum_value = np.sum(arr)    # Calculate the sum of the array
+
+# Print the results
+print("Array:", arr)
+print("Mean:", mean_value)
+print("Sum:", sum_value)
