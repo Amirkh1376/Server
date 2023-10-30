73e8a94016fa1e2cd89bf0e14cd6fecb47a2fb3f
Amirkh1376 <45887515+Amirkh1376@users.noreply.github.com>
2023-10-30 12:00:26 +0330

    Create Test folder

---
+++ Ali_Kamali/Test/Pre_install.bat
@@ -0,0 +1,16 @@
+@echo off
+
+    :: Check if Python interpreter is available
+    python --version > nul 2>&1
+    if %errorlevel% NEQ 0 (
+        echo Python interpreter not found in the system's environment.
+        echo Please install Python and ensure it is added to the system's PATH.
+        pause
+        exit /b 1
+    )
+
+    :: Install libraries using the default Python interpreter and pip
+    python -m pip install requests numpy
+
+    :: Pause to keep the command prompt window open for viewing output (optional)
+    pause
